c264422cdd4f24636ca28e9b3129e8cdfd826188
angryjim123 <angryjim123@gmail.com>
2022-03-29 14:47:12 +0800

    test

--- testing.php
+++ testing.php
@@ -1,13 +1,8 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <script src="https://app.simplefileupload.com/buckets/0f6ab5fed3a03b1d305d9ac29a4c160a.js"></script>
   </head>
   <body>
-    <form method = "POST" action = "testing.php">
-      <input type="hidden" name="avatar_url" id="avatar_url" class="simple-file-upload">
-      <input id = "submit" type="submit" value="Sign Up" >
-    </form>
     <?php
 
     $servername = "us-cdbr-east-05.cleardb.net";
@@ -15,14 +10,19 @@
     $password = "396f81cc";
     $database = "heroku_a454372d378d402";
 
-    // header("refresh:0.1;url='welcome.php'");
+    $link = $_POST['avatar_url'];
 
     try {
       $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
       // set the PDO error mode to exception
       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-      echo "hi";
+      $sql = "INSERT INTO `test_table`(`test_link`) VALUES ('$link')";
+
+      $conn->exec($sql);
+
+      $sql = null;
+      $conn = null;
       // header("refresh:0.1;url=followerspage.php");
     } catch(PDOException $e) {
       echo "Connection failed: " . $e->getMessage();
